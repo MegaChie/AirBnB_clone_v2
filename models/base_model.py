cbb4336febf606e566261e39c0ca9b0ec8b1bac1
MegaChie <workspace.mega@gmail.com>
2023-04-17 14:37:50 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -51,3 +51,8 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.key():
             del(dictionary['_sa_instance_state'])
         return dictionary
+
+    def delete(self):
+        """ Deletes the current instance from the storage. """
+        from models import storage
+        storage.delete(self)
