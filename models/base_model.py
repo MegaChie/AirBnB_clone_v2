998834ceaf3b2c70b5f94e0c2de52c29b7b1a454
MegaChie <workspace.mega@gmail.com>
2023-04-17 15:41:23 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,8 @@ from sqlalchemy import Column, String, DATETIME
 from models import storage_type
 
 Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True, unique=True)
@@ -16,7 +18,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -36,8 +37,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id,
+                                      self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
