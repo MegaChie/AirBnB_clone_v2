e4096672c0aff5ea197f6c637de0ea93bdb68e12
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-21 21:52:52 +0200

    Testing

--- models/state.py
+++ models/state.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
-
+from os import environ
 
 
 class State(BaseModel, Base):
-    """State class"""
-    __tablename__ = "states"
+    """This is the class for State
+    Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if getenv['HBNB_TYPE_STORAGE'] == 'db':
+
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
         cities = relationship('City', cascade='all, delete', backref='state')
     else:
         @property
         def cities(self):
-            """Return any city with state_id equal to self.id"""
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
             from models import storage
             from models.city import City
             # return list of City objs in __objects
             cities_dict = storage.all(City)
             cities_list = []
+
             # copy values from dict to list
             for city in cities_dict.values():
                 if city.state_id == self.id:
                     cities_list.append(city)
+
             return cities_list
