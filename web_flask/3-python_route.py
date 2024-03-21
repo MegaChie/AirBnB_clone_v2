13d5a422a9a8a9b41ac02ddbf8fda5d766dd0313
MegaChie <workspace.mega@gmail.com>
2024-03-21 12:01:38 +0000

    Task 3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask as fl
+
+app = fl(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greeting():
+    """Greets the user with a welcoming message"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNBGreeting():
+    """Returns a message on the hbnb route"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def cGame(text):
+    """
+    A small game of adding the most child route to a sentance and displaying it
+    """
+    temp = text.replace("_", " ")
+    return "C {}".format(temp)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
