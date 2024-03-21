06342ab8b1ecc67b5b6747a1ff8b7031c602f7e4
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-21 13:41:06 +0200

    Greeting message for the hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,11 @@ def greeting():
     """Greets the user with a welcoming message"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def HBNBGreeting():
+    """Returns a message on the hbnb route"""
+    return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
