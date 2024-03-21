840c2d9c3ff36811ee387685f6fd4dd47d889dcf
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-21 13:23:08 +0200

    Add greetings to the web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask as fl
+
+app = fl(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greeting():
+    """It greets the user with a welcoming message"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
