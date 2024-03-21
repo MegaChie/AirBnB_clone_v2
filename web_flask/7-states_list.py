65b09601f2817055c6aec60ce8d79546177c01c5
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-22 01:10:11 +0200

    Test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,18 +3,16 @@
 from flask import Flask as fl
 from flask import render_template as rentem
 from models import storage
-from models.state import State
 
 app = fl(__name__)
 
 
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes=False)
 def stateList():
     """Builds a list an ordered states list"""
     path = "7-states_list.html"
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return rentem(path, sorted_states=sorted_states)
+    return rentem(path, states=states)
 
 
 @app.teardown_appcontext
