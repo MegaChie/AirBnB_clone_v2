2771d5a153c1c553c9439a83ae131ddae55573a6
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-22 15:43:35 +0200

    Test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from models import storage
-from flask import Flask as fl
-from flask import render_template as rentem
+from flask import Flask
+from flask import render_template
 
-app = fl(__name__)
+app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def stateList():
-    """Builds a list an ordered states list"""
-    edit = "7-states_list.html"
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
     states = storage.all("State")
-    return rentem(edit, states=states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def SessTerm(exc):
-    """Saves list and closes session to free resources"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
