c83def16924d3be07932918934b3c946dc12f235
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-22 15:26:15 +0200

    Test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
+from models import storage
 from flask import Flask as fl
 from flask import render_template as rentem
-from models import storage
 
 app = fl(__name__)
 
@@ -10,9 +10,9 @@ app = fl(__name__)
 @app.route("/states_list", strict_slashes=False)
 def stateList():
     """Builds a list an ordered states list"""
-    path = "7-states_list.html"
-    states = storage.all(State)
-    return rentem(path, states=states)
+    edit = "7-states_list.html"
+    states = storage.all("State")
+    return rentem(edit, states=states)
 
 
 @app.teardown_appcontext
