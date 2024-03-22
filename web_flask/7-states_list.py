c36d776d923696c71eb712f15553c01c6621f322
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-22 13:24:10 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,8 +11,8 @@ app = fl(__name__)
 def stateList():
     """Builds a list an ordered states list"""
     path = "7-states_list.html"
-    states = storage.all(State)
-    return rentem(path, states=states)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return rentem(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
