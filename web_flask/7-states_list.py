ac9728908526151f3aac797a0357bb6e4b9dc240
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-22 13:34:02 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,16 +12,15 @@ def stateList():
     """Builds a list an ordered states list"""
     path = "7-states_list.html"
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
+    return rentem(path, states=states)
 
 
 @app.teardown_appcontext
-def SessTerm(arg=None):
+def SessTerm(exc):
     """Saves list and closes session to free resources"""
     storage.close()
 
 
 if __name__ == "__main__":
     app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
