781eac14fa135fdcc85e9931e13eaa675c8ffc97
MegaChie <workspace.mega@gmail.com>
2024-03-24 05:37:41 -0700

    Task 9. Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from models.state import State
+from flask import Flask as fl
+from flask import render_template as rentem
+
+app = fl(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State)
+    temp = states.values()
+    name = []
+    idNumb = []
+    for k in temp:
+        name.append(k.name)
+        idNumb.append(k.id)
+    return rentem("7-states_list.html", ids=idNumb, names=name)
+
+
+@app.teardown_appcontext
+def sessClos(arg=None):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
