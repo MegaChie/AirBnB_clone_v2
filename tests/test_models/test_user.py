133a08e06ccc3bd339be545b59d923928f9bc4cd
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-04-17 06:57:24 -0700

    Console Improvement

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = 'Julius'
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = 'Irving'
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        new.email = 'thedoctor@gmail.com'
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = 'drJ6'
         self.assertEqual(type(new.password), str)
