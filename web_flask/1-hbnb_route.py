2c069a3341262bb65c13fdd0d832f1b8d4ded31f
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-05-18 07:36:39 -0700

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a Flask web application listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print/display HBNB for /hbnb"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
