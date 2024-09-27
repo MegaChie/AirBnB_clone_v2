dc67f449faf2351e6bb419cdb878ad5f2af27c10
usanas7 <c.usanase@alustudent.com>
2024-09-27 06:15:12 -0700

    places

--- models/place.py
+++ models/place.py
@@ -35,37 +35,3 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 viewonly=False,
-                                 back_populates="place_amenities")
-
-    if os.getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def reviews(self):
-            """Returns the list of Review instances with place_id equals
-            to the current Place.id."""
-
-            reviews = list(models.storage.all(Review).values())
-
-            return list(
-                filter(lambda review: (review.place_id == self.id), reviews))
-
-        @property
-        def amenities(self):
-            """Returns the list of Amenity instances based on
-            the attribute amenity_ids that contains all Amenity.id."""
-
-            amenities = list(models.storage.all(Amenity).values())
-
-            return list(
-                filter(lambda amenity: (amenity.place_id in self.amenity_ids),
-                       amenities))
-
-        @amenities.setter
-        def amenities(self, value=None):
-            """Adds ids in amenity_ids ."""
-            if type(value) == type(Amenity):
-                self.amenity_ids.append(value.id)
