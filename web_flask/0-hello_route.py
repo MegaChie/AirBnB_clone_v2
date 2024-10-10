1a78fd4e4b5574f33571a4afc2f8523fa91f3ddb
tonywahome <a.wambugu@alustudent.com>
2024-10-10 11:52:00 +0200

    Add /hbnb route to Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Start a Flask web application
+Routes:
+/hbnb: display “Hello HBNB!"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+if __name__ == " __main__":
+    app.run(host='0.0.0.0')
