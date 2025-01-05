4cb0d8aa194f564c3e021beb796f596807acd99c
ouidane <ouidanezakaria@gmail.com>
2025-01-05 03:35:16 +0100

    add test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ """
+import os
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
@@ -8,12 +9,15 @@ class test_state(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize the test class for State."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """Tests the type of name attribute."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
