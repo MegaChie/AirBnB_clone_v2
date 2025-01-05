0ddbb32a8f5e70e82e67ac46c6a24b7f23f2fc54
ouidane <ouidanezakaria@gmail.com>
2025-01-05 03:35:14 +0100

    add amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
 
 
 class Amenity(BaseModel):
     """This is the class for Amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        place_amenities = relationship("Place", secondary="place_amenity")
