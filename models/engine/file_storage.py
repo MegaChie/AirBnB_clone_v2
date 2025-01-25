2e89ccffdc8580b83bffd8ddba837d6b0b5fb809
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2025-01-25 21:06:24 +0200

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,65 +1,62 @@
 #!/usr/bin/python3
-"""
-FileStorage class that serializes instances to a 
-JSON file and deserializes JSON file to instances"""
-
+"""This is the file storage class for AirBnB"""
 import json
-from models.user import User
 from models.base_model import BaseModel
+from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import shlex
+
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
-
-    __file_path = "storage.json"
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of all objects, or filtered by class if cls is provided."""
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
         if cls:
-            cls_name = cls.__name__
-            return {key: obj for key, obj in self.__objects.items() if key.startswith(cls_name)}
-        return self.__objects
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
-        for key, value in self.__objects.items():
-            print(f"Key: {key}, Value: {value}")  # Debugging statement
-            class_name = value['__class__']
-            print(f"Class name: {class_name}")  # Debugging statement
-            class_map = {
-                'BaseModel': BaseModel,
-                'User': User,
-                'State': State,
-                'City': City,
-                'Amenity': Amenity,
-                'Place': Place,
-                'Review': Review
-            }
-            if class_name in class_map:
-                self.__objects[key] = class_map[class_name](**value)
-            else:
-                print(f"Class {class_name} not found in class_map")
+        """serialize the file path to JSON file path
+        """
 
-    def delete(self, obj=None):
-        """Deletes obj from __objects if it exists."""
-        if obj is not None:
-            key = f"{obj.__class__.__name__}.{obj.id}"
-            if key in self.__objects:
-                del self.__objects[key]
-                self.save()
