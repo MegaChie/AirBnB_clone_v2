e78442ee5a816958bdc8641f3443cf29a1bf56b3
gacenga <jimwillymurigu@gmail.com>
2025-02-07 17:14:22 +0000

    flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""simple flask app
 """
-   Simple flask app
+from flask import Flask
+app = Flask(__name__)
 
-"""
 
-from web_flask import create_app
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
 
-app = create_app()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
