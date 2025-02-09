33c9cf831b22adc7a0cac7c5b9f33fb0a4a96ea7
gacenga <jimwillymurigu@gmail.com>
2025-02-09 12:56:31 +0000

    flask app

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+   simple flask app
+
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """ intro """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ name """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ display C followed by text """
+    return f"C {text.replace('_', ' ')}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ display Python followed by text """
+    return f"Python {text.replace('_', ' ')}"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_n(n):
+    """ display n is number when n is integer """
+    return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_page(n):
+    """ render page when n is integer """
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def html_page_2(n):
+    """ render page when n is integer """
+    x = "even" if (n % 2) == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, x=x)
+
+@app.route('/states_list', strict_slashes=False)
+def  html_page_3():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda: state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
