cc5c224e7fdcb74bb94f3e89000cd9c5491a6c87
gacenga <jimwillymurigu@gmail.com>
2025-02-09 13:57:59 +0000

    flask app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,21 +5,20 @@
 """
 
 from flask import Flask, render_template
-from models import storage
 from models import *
+from models import storage
 
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def  html_page_3():
     """ fetching data """
-    states = list(storage.all("State").values())
-    sorted_states = sorted(states, key=lambda: x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = sorted( list(storage.all("State").values()), lambda: x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
 def close_storage(exception):
-    """ remove currnet SQLAlchemy session """
+    """ remove current SQLAlchemy session """
     storage.close()
 
 if __name__ == "__main__":
