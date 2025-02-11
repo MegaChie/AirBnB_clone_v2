5a84e37c52407271d14270ce0e0f702c4b04be33
ouidane <ouidanezakaria@gmail.com>
2025-02-11 01:59:21 +0100

    update

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -28,15 +28,9 @@ def states_cities_list():
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    Close the database connection after each request.
-
-    Args:
-        exception: Any exception that occurred during the request handling.
-    """
+    """close the storage on teardown db"""
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
