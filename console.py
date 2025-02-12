d534c1e7a511d17424e8fcbdbe4c5ea7985d30a6
mwanzaalbert <user.email>
2025-02-12 07:01:47 +0300

    refactor(console.py): Refactored exit() and print() with return True in do_EOF method

--- console.py
+++ console.py
@@ -102,8 +102,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
-        print()
-        exit()
+        return True
 
     def help_EOF(self):
         """ Prints the help documentation for EOF """
