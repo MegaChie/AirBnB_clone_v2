9303c938763d67828d1766b4e9044c084f2c4e38
mwanzaalbert <user.email>
2025-02-12 08:30:06 +0300

    refactor(models/engine/file_storage.py): Refactored save method to use dictionary comprehension

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,12 +47,13 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        with open(self.__file_path, 'w', encoding='utf-8') as outfile:
+            temp = {
+                key: value.to_dict()
+                for key, value in self.__objects.items()
+                }
+            
+            json.dump(temp, outfile)
 
     def reload(self):
         """Loads storage dictionary from file"""
