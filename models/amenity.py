764f1696da024773b87ef5f6dd61d39d82e37504
mwanzaalbert <user.email>
2025-02-12 10:38:06 +0300

    feat(models/amenity.py): Updated class Amenity to Inherit from BaseModel and Base. Added class attributes for SQLAlchemy table mapping and Many to Many relationship mapping with Places

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+# -*- coding: utf-8 -*-
+"""
+Amenity module for the hbnb project.
+
+Defines the `Amenity` class, which represents amenities available for places.
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Represents an amenity in the hbnb project.
+
+    Inherits from_:
+        - BaseModel: Provides common attributes like `id`, `created_at`, and
+          `updated_at`.
+        - Base: Enables SQLAlchemy ORM functionality.
+
+    Attributes_:
+        __tablename__ (str): The name of the MySQL table.
+        name (Column or str): The name of the amenity.
+        place_amenities (relationship): A many-to-many relationship with
+                                        `Place` (only for DB storage).
+    """
+
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       overlaps="place_amenities")
+    else:
+        name: str = ""
