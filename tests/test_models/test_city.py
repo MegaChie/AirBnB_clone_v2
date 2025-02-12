de81e99c9d5f328cb40918888ab70102f79f889c
mwanzaalbert <user.email>
2025-02-12 14:01:11 +0300

    docs(tests/test_models/city.py): Added docstrings

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" """
+"""Unit tests for City"""
 from tests.test_models.test_base_model import TestBaseModel
 from models.city import City
 
 
-class test_City(TestBaseModel):
-    """ """
+class TestCity(TestBaseModel):
+    """Test cases for the City class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize test class"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """
+        Test that state_id exists, is a string, and defaults to an empty string
+        """
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertTrue(hasattr(new, "state_id"))
+        self.assertIsInstance(new.state_id, str)
+        self.assertEqual(new.state_id, "")
 
     def test_name(self):
-        """ """
+        """
+        Test that name exists, is a string, and defaults to an empty string
+        """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertTrue(hasattr(new, "name"))
+        self.assertIsInstance(new.name, str)
+        self.assertEqual(new.name, "")
