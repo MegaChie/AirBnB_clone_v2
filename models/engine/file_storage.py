fcedbb650d4eca90d2952dabdd50f77812893c9e
mwanzaalbert <user.email>
2025-02-12 08:48:17 +0300

    style(models/engine/file_storage.py): Pycodestyle Formatting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,62 +1,63 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for hbnb clone."""
 import json
 import importlib
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of hbnb models in JSON format."""
+
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Return a dictionary of models currently in storage."""
         classes = {'BaseModel',
-                'User', 'Place',
-                'State', 'City', 'Amenity',
-                'Review'
-              }
-        
+                   'User', 'Place',
+                   'State', 'City', 'Amenity',
+                   'Review'
+                   }
+
         if cls is not None:
             if isinstance(cls, str):
                 if cls not in classes:
                     return {}
                 try:
-                    # Dynamically import the class
-                    module_path = f"models.{cls.lower()}"  # Convert class name to module name
+                    # Dynamically import the class and
+                    # Convert class name to module name
+                    module_path = f"models.{cls.lower()}"
                     module = importlib.import_module(module_path)
                     cls = getattr(module, cls)  # Get the class from the module
-                    
+
                 except (ImportError, AttributeError):
                     print("** class doesn't exist **")
-                
-                    
+
             # Filter objects based on the class
             return {
                 key: value
                 for key, value in self.__objects.items()
                 if isinstance(value, cls)
-                }
-                
+            }
+
         return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Add new object to storage dictionary."""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Save storage dictionary to file."""
         with open(self.__file_path, 'w', encoding='utf-8') as outfile:
             temp = {
                 key: value.to_dict()
                 for key, value in self.__objects.items()
-                }
-            
+            }
+
             json.dump(temp, outfile)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load storage dictionary from file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -66,14 +67,14 @@ class FileStorage:
         from models.review import Review
 
         classes = {'BaseModel': BaseModel,
-                    'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+                   'User': User, 'Place': Place,
+                   'State': State, 'City': City, 'Amenity': Amenity,
+                   'Review': Review
+                   }
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as infile:
                 file_data = json.load(infile)
-                
+
                 for key, value in file_data.items():
                     cls_name = value['__class__']
                     self.__objects[key] = classes[cls_name](**value)
