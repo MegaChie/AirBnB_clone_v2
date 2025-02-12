a731604dc089edffa3b4863fdd96fc4d653c8cc5
mwanzaalbert <user.email>
2025-02-12 14:42:36 +0300

    test(tests/test_models/state.py): Added docstrings and additional assertions for test_name3

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for the State class"""
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
 
-class test_state(test_basemodel):
-    """ """
+class test_state(TestBaseModel):
+    """Test cases for the State class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize test class"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """Test that name exists and is a string"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertTrue(hasattr(new, "name"))
+        self.assertIsInstance(new.name, str)
+        self.assertEqual(new.name, "")  # Default value check
