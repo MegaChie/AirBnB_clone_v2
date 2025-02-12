d78801164042c4308baa832ec00bbb8bc922b554
mwanzaalbert <user.email>
2025-02-12 09:46:22 +0300

    refactor(models/base_model.py): Refactored __init__ to iterate over kwargs and update attributes using setattr function. Removed storage import from __init__ and calling of the storage.new method

--- models/base_model.py
+++ models/base_model.py
@@ -22,21 +22,22 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
+        """Instantiates a new model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key,
+                                datetime.strptime(kwargs[f'{key}'],
+                                                  '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+                        
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
