a2cbcac86ad913badb8a23cb7b073dbfcf93f10c
mwanzaalbert <user.email>
2025-02-12 09:32:36 +0300

    feat(models/basem_model.py): Added class attributes for SQLAlchemy table mapping

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """This module defines a base class for all models in our hbnb clone"""
+import os
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    __abstract__ = True
+
+    # Mark this class as an abstract to prevent its table creation
+    id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
