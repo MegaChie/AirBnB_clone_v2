166e957893ea3056c852c9c7d8b6176826cf70f8
mwanzaalbert <user.email>
2025-02-12 11:33:21 +0300

    feat(model/review.py): Updated class with attributes for SQLAlchemy table mapping

--- models/review.py
+++ models/review.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+# -*- coding: utf-8 -*-
+"""
+Review module for the HBNB project.
+
+Defines the `Review` class, which stores user reviews related to places.
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Represents a review given by a user for a specific place.
+
+    Inherits from_:
+        - BaseModel: Provides common attributes like `id`, `created_at`, and
+                     `updated_at`.
+        - Base: Enables SQLAlchemy ORM functionality.
+
+    Attributes_:
+        __tablename__ (str): The name of the MySQL table for reviews.
+        place_id (Column or str): The ID of the associated place (foreign key,
+                                                                  required).
+        user_id (Column or str): The ID of the user who wrote the review
+                                (foreign key, required).
+        text (Column or str): The review content (string, required).
+    """
+
+    __tablename__ = "reviews"
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False, )
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") != 'db':
+        place_id: str = ""
+        user_id: str = ""
+        text: str = ""
